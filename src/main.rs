2ed11e9375ab20c5ff24a1da6721acf14bce9b3e
Anup Dhamala <anupdhml+git@gmail.com>
2020-05-27 17:06:16 -0400

    Add in all the allocator options
    Makes for a more intresting build exercise due to all the C dependencies allocators pull

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[cfg(feature = "allocator-snmalloc")]
+#[global_allocator]
+static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
+
+#[cfg(feature = "allocator-mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[cfg(feature = "allocator-jemalloc")]
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
 fn main() {
     println!("Hello, world!");
 }
