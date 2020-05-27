3a37e25d5b0d196c8c0f0d9e35dfd87c32d44678
Anup Dhamala <anupdhml+git@gmail.com>
2020-05-27 17:07:00 -0400

    Build across various targets via cross

---
+++ builder/README.md
@@ -0,0 +1,15 @@
+Docker images used for compilation across various targets in CI builds (or locally if desired). Meant for use with [cross](https://github.com/rust-embedded/cross), but can be used independent of it too.
+
+The images here are picked up via the [cross configuration](../Cross.toml) file at project root, during builds.
+
+If you are changing any of the files here, you will need to rebuild and push the image:
+
+```
+# build image for a target
+./build_image.sh x86_64-unknown-linux-gnu
+
+# push the image
+docker push anupdhml/example-builder:x86_64-unknown-linux-gnu
+```
+
+The images are provisoned with the rust version specified in the project [rust-toolchain](../rust-toolchain) file, so the above instructions apply for when we bump the versions there too.
---
+++ builder/build_image.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# build_image.sh
+#
+# Build docker images used for building this rust project
+#
+# Meant to be run from the same directory as the script
+#
+# Usage: ./build_image.sh TARGET
+# Example: ./build_image.sh x86_64-unknown-linux-gnu
+
+# exit the script when a command fails
+set -o errexit
+
+# catch exit status for piped commands
+set -o pipefail
+
+TARGET=$1
+if [ -z "$TARGET" ]; then
+  echo "Usage: build_image.sh TARGET"
+  exit 1
+fi
+
+DOCKERFILE="Dockerfile.${TARGET}"
+if [ ! -f "$DOCKERFILE" ]; then
+  echo "A Dockerfile does not exist for the specified target: ${TARGET}"
+  exit 1
+fi
+
+IMAGE_NAMESPACE="anupdhml"
+IMAGE_NAME="example-builder"
+
+RUST_TOOLCHAIN_FILE="../rust-toolchain"
+RUST_VERSION=$(<"$RUST_TOOLCHAIN_FILE")
+
+docker build \
+  --network host \
+  -t "${IMAGE_NAMESPACE}/${IMAGE_NAME}:${TARGET}" \
+  -t "${IMAGE_NAMESPACE}/${IMAGE_NAME}:${TARGET}-${RUST_VERSION}" \
+  --build-arg RUST_VERSION=${RUST_VERSION} \
+  -f ${DOCKERFILE} \
+  .
